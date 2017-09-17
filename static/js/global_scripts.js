afce124ade54a1e51e6be63e55a08cd9a2732318
Yogendra Tamang <48yogen@gmail.com>
2017-09-17 18:46:17 +0545

    Scripts Added

--- static/js/global_scripts.js
+++ static/js/global_scripts.js
@@ -1,4 +1,18 @@
 $(function(){
+$('#btnClearAll').on('click', function(){
+    console.log("Clearing....")
+    $.ajax({
+        url: "/clear_session/",
+        method: 'POST', // or another (GET), whatever you need
+        data: {
+            "clear_all": 1 // data you need to pass to your function
+        },
+        success: function (data) {
+
+        }
+    });
+});
+
 $('#sumColumnEWTGroup').hide();
 $('#aggregationTypeEWT').on('change',(function() {
             if($('#aggregationTypeEWT option:selected').val()==='SUM')
